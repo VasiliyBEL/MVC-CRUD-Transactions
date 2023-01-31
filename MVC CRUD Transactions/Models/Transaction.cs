b005cc8d3c86f56be7644b1d72a3fe86749570e0
VasiliyBEL <vaspn001@gmail.com>
2023-01-31 22:02:33 +0300

    Edit Transaction model with attributes.

--- MVC CRUD Transactions/Models/Transaction.cs
+++ MVC CRUD Transactions/Models/Transaction.cs
@@ -7,23 +7,35 @@ namespace MVC_CRUD_Transactions.Models
 {
     public class Transaction
     {
-            [Key]
-            public int TransactionId { get; set; }
+        [Key]
+        public int TransactionId { get; set; }
 
-            [Column(TypeName = "nvarchar(12)")]
-            public string AccountNumber { get; set; }
+        [Column(TypeName = "nvarchar(12)")]
+        [DisplayName("Account Number")]
+        [Required(ErrorMessage = "This field is required.")]
+        [MaxLength(12, ErrorMessage = "Maximum 12 characters only.")]
+        public string AccountNumber { get; set; }
 
-            [Column(TypeName = "nvarchar(100)")]
-            public string BeneficiaryName { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Beneficiary Name")]
+        [Required(ErrorMessage = "This field is required.")]
+        public string BeneficiaryName { get; set; }
 
-            [Column(TypeName = "nvarchar(100)")]
-            public string BankName { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Bank Name")]
+        [Required(ErrorMessage = "This field is required.")]
+        public string BankName { get; set; }
 
-            [Column(TypeName = "nvarchar(11)")]
-            public string SWIFTCode { get; set; }
+        [Column(TypeName = "nvarchar(11)")]
+        [DisplayName("SWIFT Code")]
+        [Required(ErrorMessage = "This field is required.")]
+        [MaxLength(11, ErrorMessage = "Maximum 11 characters only.")]
+        public string SWIFTCode { get; set; }
 
-            public int Amount { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public int Amount { get; set; }
 
-            public DateTime Date { get; set; }
-        }
+        [DisplayFormat(DataFormatString = "{0:MMM-dd-yy}")]
+        public DateTime Date { get; set; }
+    }
 }
