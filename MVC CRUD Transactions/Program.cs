943a165842443e1cba15424cf61163c391c046fc
VasiliyBEL <vaspn001@gmail.com>
2023-01-31 22:07:05 +0300

    edit MapControllerRoute pattern to Transaction controller.

--- MVC CRUD Transactions/Program.cs
+++ MVC CRUD Transactions/Program.cs
@@ -25,6 +25,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Transaction}/{action=Index}/{id?}");
 
 app.Run();
